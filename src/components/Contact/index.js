a6a63d853477774ec587db3fe86b15aa06bac139
Gongjjin <gjyj5395@naver.com>
2025-08-12 09:00:22 +0900

    hotfix : 문의하기 미사용 변수를 await으로 처리

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -141,7 +141,7 @@ const Contact = () => {
 
     try {
       // EmailJS를 사용하여 이메일 전송
-      const result = await emailjs.send(
+      await emailjs.send(
         process.env.REACT_APP_EMAILJS_SERVICE_ID,
         process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
         {
