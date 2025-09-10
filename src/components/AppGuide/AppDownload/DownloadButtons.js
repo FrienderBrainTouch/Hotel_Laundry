3ff7b33a028a07eac04490d77a7e646567932842
haewonwon <khcw1029@daum.net>
2025-09-10 16:31:21 +0900

    fix: ESLint 경고 수정 - DownloadButtons href 속성 제거
    - href='#' 속성을 button 요소로 변경
    - onClick 핸들러로 외부 링크 열기 구현
    - Netlify CI 빌드 에러 해결

--- src/components/AppGuide/AppDownload/DownloadButtons.js
+++ src/components/AppGuide/AppDownload/DownloadButtons.js
@@ -5,20 +5,20 @@ const DownloadButtons = () => {
     <section className="py-12 bg-[#102254]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col sm:flex-row gap-6 justify-center">
-          <a
-            href="#"
+          <button
             className="flex-1 inline-flex items-center justify-center px-12 py-6 bg-white text-[#102254] rounded-xl font-bold text-20 hover:bg-gray-100 transition-colors min-h-[60px]"
+            onClick={() => window.open('https://apps.apple.com', '_blank')}
           >
             <span className="mr-4 text-28">📱</span>
             App Store에서 다운로드
-          </a>
-          <a
-            href="#"
+          </button>
+          <button
             className="flex-1 inline-flex items-center justify-center px-12 py-6 bg-white text-[#102254] rounded-xl font-bold text-20 hover:bg-gray-100 transition-colors min-h-[60px]"
+            onClick={() => window.open('https://play.google.com', '_blank')}
           >
             <span className="mr-4 text-28">🤖</span>
             Google Play에서 다운로드
-          </a>
+          </button>
         </div>
       </div>
     </section>
