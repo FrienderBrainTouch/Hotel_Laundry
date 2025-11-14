0b61ed4031b8f0ea5d398cfdcfbbbdf7926c4656
Gongjjin <gjyj5395@naver.com>
2025-11-14 11:17:03 +0900

    feat: 모바일에서도 여백 없애기

--- src/components/StartupGuide/LowCapitalStartup/index.js
+++ src/components/StartupGuide/LowCapitalStartup/index.js
@@ -41,19 +41,17 @@ const LowCapitalStartup = () => {
             />
 
             {/* 유튜브 영상 */}
-            <div className="w-full py-8 sm:py-12 md:py-16">
-              <div className="max-w-5xl mx-auto px-4">
-                <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
-                  <iframe 
-                    className="absolute top-0 left-0 w-full h-full rounded-lg shadow-lg"
-                    src="https://www.youtube.com/embed/XujIAFTmirM?si=un3ebSyPfyyvF634" 
-                    title="YouTube video player" 
-                    frameBorder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                    referrerPolicy="strict-origin-when-cross-origin" 
-                    allowFullScreen
-                  />
-                </div>
+            <div className="w-full">
+              <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
+                <iframe 
+                  className="absolute top-0 left-0 w-full h-full"
+                  src="https://www.youtube.com/embed/XujIAFTmirM?si=un3ebSyPfyyvF634" 
+                  title="YouTube video player" 
+                  frameBorder="0" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                  referrerPolicy="strict-origin-when-cross-origin" 
+                  allowFullScreen
+                />
               </div>
             </div>
 
